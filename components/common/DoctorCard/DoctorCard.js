21285a59aa9c54f86d8b7e42845e00a7c2120dba
Esenushka <prosterling.mien@gmail.com>
2022-04-05 19:44:29 +0600

    fix

--- components/common/DoctorCard/DoctorCard.js
+++ components/common/DoctorCard/DoctorCard.js
@@ -1,27 +1,30 @@
 import css from "../../../styles/doctorCard.module.css"
 
-export default function DoctorCard({list}) {
+export default function DoctorCard({ list }) {
     return (
-        <div className="doctor_card_wrapper">
-            {
-                list.map((e)=>
-                    <div key={e.id} className={css.wrapper}>
-                        <img src={e.img} />
-                        <div>
-                            <div className={css.name}>{e.name}</div>
+        <div>
+            <div className="doctor_card_wrapper">
+                {
+                    list.map((e) =>
+                        <div key={e.id} className={css.wrapper}>
+                            <img src={e.img} />
                             <div>
-                                <div className={css.location}>{e.location}</div>
-                                <div className={css.post}>{e.post}</div>
-                                <div className={css.proffesion}>{e.proffesion}</div>
+                                <div className={css.name}>{e.name}</div>
+                                <div>
+                                    <div className={css.location}>{e.location}</div>
+                                    <div className={css.post}>{e.post}</div>
+                                    <div className={css.proffesion}>{e.proffesion}</div>
+                                </div>
+                                <button className="btn btn-blue">Записаться на прием</button>
                             </div>
-                            <button className="btn btn-blue">Записаться на прием</button>
                         </div>
-                    </div>
-                )
-            }
-          <div style={{ display: "flex", justifyContent: "center", width: "100%" }}>
-            <button className="big-btn btn btn-white">Посмотреть всех специалистов</button>
-          </div>
+                    )
+                }
+
+            </div>
+            <div style={{marginBottom: "50px" , display: "flex", justifyContent: "center", width: "100%" }}>
+                <button className="big-btn btn btn-white">Посмотреть всех специалистов</button>
+            </div>
         </div>
     )
 }
